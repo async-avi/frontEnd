4f5f069e1926e1baf16a5deb6648a9f7b4fe7db7
avi <avi.contact22@gmail.com>
2024-01-29 18:16:59 +0530

    implemented scrollTrigger with gsap

--- animation/gsap/basic/scrollTrigger/index.js
+++ animation/gsap/basic/scrollTrigger/index.js
@@ -1,23 +1,9 @@
-// gsap.to(".content h1", {
-//   x: -2000,
-//   scrollTrigger: {
-//     markers: true,
-//     start: "top 0",
-//     end: "top -100%",
-//     trigger: ".div2",
-//     scroller: "body",
-//     scrub: 0.5,
-//     pin: true,
-//   },
-// });
-
 gsap.to(".div2 h1", {
   x: "-200%",
   scrollTrigger: {
     trigger: ".div2",
-    markers: true,
     start: "top 0",
-    end: "top -100%",
+    end: "top -200%",
     pin: true,
     scrub: 2,
   },
